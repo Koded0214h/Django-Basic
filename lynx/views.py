50a327a1150ac8983bcab0555db12812b4d17830
Raufu-abdulrahman <abdulrahmanraufu@gmail.coml>
2024-12-10 21:53:44 +0100

    Android

--- lynx/views.py
+++ lynx/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(response):
-    return HttpResponse('<h1>Connected with Django</h1>')
+    return HttpResponse("<h1>Connected with Django</h1> <h3>It looks good on Android Mobile</h3>")
